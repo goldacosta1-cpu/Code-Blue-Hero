6f03541591f3671a7904da74d01763ff6286a8ea
Lawrence Gimenez <law@gmnz.xyz>
2025-12-04 14:07:23 +0800

    Initial commit

---
+++ CodeBlueHero/CodeBlueHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBlueHeroApp.swift
+//  CodeBlueHero
+//
+//  Created by Lawrence Gimenez on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeBlueHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBlueHeroTests/CodeBlueHeroTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBlueHeroTests.swift
+//  CodeBlueHeroTests
+//
+//  Created by Lawrence Gimenez on 12/4/25.
+//
+
+import Testing
+@testable import CodeBlueHero
+
+struct CodeBlueHeroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
