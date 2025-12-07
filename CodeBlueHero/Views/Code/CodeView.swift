047215ea3113596d0a61bd62248c1ee22380eca7
Lawrence Gimenez <law@gmnz.xyz>
2025-12-07 15:07:18 +0800

    WIP: Start CPR Cycle button

--- CodeBlueHero/Views/Code/CodeView.swift
+++ CodeBlueHero/Views/Code/CodeView.swift
@@ -43,9 +43,30 @@ struct CodeView: View {
                     .background(.backgroundBlue)
                     .clipShape(Capsule())
                 }
+                // Start CPR Cycle
+                Button {
+                    
+                } label: {
+                    VStack(spacing: 8) {
+                        Image(systemName: "timer.circle.fill")
+                            .frame(width: 40, height: 40)
+                            .foregroundStyle(.white)
+                        Text("START CPR CYCLE")
+                            .font(.system(size: 35))
+                            .foregroundStyle(.white)
+                        Text("Tap to begin 2-minute timer")
+                            .font(.system(size: 12))
+                            .foregroundStyle(.white)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(24)
+                    .background(.backgroundGreen500)
+                    .clipShape(RoundedRectangle(cornerRadius: 24))
+                }
                 Spacer()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.horizontal, 16)
             .onReceive(timer) { date in
                 timeElapsed = Int(date.timeIntervalSince(currentDate))
             }
