799890518d54d9b7adfaa4e7fcb9aab6895f94f4
Lawrence Gimenez <law@gmnz.xyz>
2025-12-12 07:45:46 +0800

    Spacing adjustments and added scrollview

--- CodeBlueHero/Views/Code/CodeView.swift
+++ CodeBlueHero/Views/Code/CodeView.swift
@@ -23,187 +23,189 @@ struct CodeView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("CODE TIME")
-                    .font(.system(size: 12))
-                    .foregroundStyle(.textGray)
-                    .fontWeight(.semibold)
-                    .padding(.top, 16)
-                Text(formattedTimerString)
-                    .font(.system(size: 40))
-                    .monospacedDigit()
-                    .bold()
-                // Share Timer
-                Button {
-                    
-                } label: {
-                    HStack {
-                        Image(systemName: "square.and.arrow.up")
-                            .foregroundStyle(.white)
-                        Text("Share Timer")
-                            .foregroundStyle(.white)
+            ScrollView {
+                VStack(spacing: 16) {
+                    Text("CODE TIME")
+                        .font(.system(size: 12))
+                        .foregroundStyle(.textGray)
+                        .fontWeight(.semibold)
+                        .padding(.top, 32)
+                    Text(formattedTimerString)
+                        .font(.system(size: 40))
+                        .monospacedDigit()
+                        .bold()
+                    // Share Timer
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Image(systemName: "square.and.arrow.up")
+                                .foregroundStyle(.white)
+                            Text("Share Timer")
+                                .foregroundStyle(.white)
+                        }
+                        .padding(.vertical, 12)
+                        .padding(.horizontal, 24)
+                        .background(.backgroundBlue)
+                        .clipShape(Capsule())
                     }
-                    .padding(.vertical, 12)
-                    .padding(.horizontal, 24)
-                    .background(.backgroundBlue)
-                    .clipShape(Capsule())
-                }
-                // Start CPR Cycle
-                Button {
-                    
-                } label: {
-                    VStack(spacing: 8) {
-                        Image(systemName: "timer.circle.fill")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                            .foregroundStyle(.white)
-                        Text("START CPR CYCLE")
-                            .font(.system(size: 24))
-                            .foregroundStyle(.white)
-                        Text("Tap to begin 2-minute timer")
-                            .font(.system(size: 14))
-                            .foregroundStyle(.white)
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(24)
-                    .background(.backgroundGreen500)
-                    .clipShape(RoundedRectangle(cornerRadius: 24))
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 24).stroke(Color(.borderGreen600), lineWidth: 4)
-                    }
-                }
-                // First Epi Dose & Time Since Shock
-                HStack {
-                    VStack(spacing: 8) {
-                        Image(systemName: "pill")
-                            .resizable()
-                            .frame(width: 28, height: 28)
-                        Text("FIRST EPI DOSE")
-                            .foregroundStyle(.textGray)
-                            .font(.system(size: 12))
-                        Text("Not Due Yet")
-                            .font(.system(size: 18))
-                            .foregroundStyle(.textGray)
-                    }
-                    .frame(maxWidth: .infinity, minHeight: 100, maxHeight: 100)
-                    .padding(16)
-                    .clipShape(RoundedRectangle(cornerRadius: 24))
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 24).stroke(Color(.borderGray300), lineWidth: 2)
-                    }
-                    VStack {
+                    // Start CPR Cycle
+                    Button {
+                        
+                    } label: {
                         VStack(spacing: 8) {
-                            Image(systemName: "bolt.fill")
-                            Text("TIME SINCE SHOCK")
-                                .foregroundStyle(.textGray)
+                            Image(systemName: "timer.circle.fill")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundStyle(.white)
+                            Text("START CPR CYCLE")
+                                .font(.system(size: 24))
+                                .foregroundStyle(.white)
+                            Text("Tap to begin 2-minute timer")
+                                .font(.system(size: 14))
+                                .foregroundStyle(.white)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(24)
+                        .background(.backgroundGreen500)
+                        .clipShape(RoundedRectangle(cornerRadius: 24))
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 24).stroke(Color(.borderGreen600), lineWidth: 4)
+                        }
+                    }
+                    // First Epi Dose & Time Since Shock
+                    HStack {
+                        VStack(spacing: 8) {
+                            Image(systemName: "pill")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                            Text("FIRST EPI DOSE")
                                 .foregroundStyle(.textGray)
                                 .font(.system(size: 12))
+                            Text("Not Due Yet")
+                                .font(.system(size: 18))
+                                .foregroundStyle(.textGray)
                         }
                         .frame(maxWidth: .infinity, minHeight: 100, maxHeight: 100)
                         .padding(16)
+                        .clipShape(RoundedRectangle(cornerRadius: 24))
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 24).stroke(Color(.borderGray300), lineWidth: 2)
+                        }
+                        VStack {
+                            VStack(spacing: 8) {
+                                Image(systemName: "bolt.fill")
+                                Text("TIME SINCE SHOCK")
+                                    .foregroundStyle(.textGray)
+                                    .foregroundStyle(.textGray)
+                                    .font(.system(size: 12))
+                            }
+                            .frame(maxWidth: .infinity, minHeight: 100, maxHeight: 100)
+                            .padding(16)
+                        }
+                        .background(.backgroundGray100)
+                        .clipShape(RoundedRectangle(cornerRadius: 24))
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 24).stroke(Color(.borderGray300), lineWidth: 2)
+                        }
                     }
+                    // Rhythm Check
+                    VStack(spacing: 10) {
+                        HStack {
+                            Image(.pulse)
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundStyle(.textGray)
+                            Text("RHYTHM CHECK")
+                                .font(.system(size: 14))
+                                .foregroundStyle(.textGray600)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("SHOCKABLE RHYTHMS")
+                            .font(.system(size: 12))
+                            .bold()
+                            .foregroundStyle(.textOrange600)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        HStack {
+                            Button {
+                                
+                            } label: {
+                                Text("VF")
+                                    .frame(maxWidth: .infinity, minHeight: 44, maxHeight: 44)
+                                    .background(.backgroundRed500)
+                                    .foregroundStyle(.white)
+                                    .font(.system(size: 14))
+                                    .bold()
+                                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 16).stroke(Color(.borderRed600), lineWidth: 2)
+                                    }
+                            }
+                            Button {
+                                
+                            } label: {
+                                Text("VTach")
+                                    .frame(maxWidth: .infinity, minHeight: 44, maxHeight: 44)
+                                    .background(.backgroundRed500)
+                                    .foregroundStyle(.white)
+                                    .font(.system(size: 14))
+                                    .bold()
+                                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 16).stroke(Color(.borderRed600), lineWidth: 2)
+                                    }
+                            }
+                        }
+                        Text("NON-SHOCKABLE RHYTHMS")
+                            .foregroundStyle(.textBlue600)
+                            .font(.system(size: 12))
+                            .bold()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        HStack {
+                            Button {
+                                
+                            } label: {
+                                Text("PEA")
+                                    .frame(maxWidth: .infinity, minHeight: 44, maxHeight: 44)
+                                    .background(.backgroundGray300)
+                                    .foregroundStyle(.textGray900)
+                                    .font(.system(size: 14))
+                                    .bold()
+                                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 16).stroke(Color(.borderGray400), lineWidth: 2)
+                                    }
+                            }
+                            Button {
+                                
+                            } label: {
+                                Text("Asystole")
+                                    .frame(maxWidth: .infinity, minHeight: 44, maxHeight: 44)
+                                    .background(.backgroundGray300)
+                                    .foregroundStyle(.textGray900)
+                                    .font(.system(size: 14))
+                                    .bold()
+                                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 16).stroke(Color(.borderGray400), lineWidth: 2)
+                                    }
+                            }
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(16)
                     .background(.backgroundGray100)
                     .clipShape(RoundedRectangle(cornerRadius: 24))
                     .overlay {
                         RoundedRectangle(cornerRadius: 24).stroke(Color(.borderGray300), lineWidth: 2)
                     }
+                    Spacer()
                 }
-                // Rhythm Check
-                VStack {
-                    HStack {
-                        Image(.pulse)
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                            .foregroundStyle(.textGray)
-                        Text("RHYTHM CHECK")
-                            .font(.system(size: 14))
-                            .foregroundStyle(.textGray600)
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("SHOCKABLE RHYTHMS")
-                        .font(.system(size: 12))
-                        .bold()
-                        .foregroundStyle(.textOrange600)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    HStack {
-                        Button {
-                            
-                        } label: {
-                            Text("VF")
-                                .frame(maxWidth: .infinity, minHeight: 44, maxHeight: 44)
-                                .background(.backgroundRed500)
-                                .foregroundStyle(.white)
-                                .font(.system(size: 14))
-                                .bold()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 16).stroke(Color(.borderRed600), lineWidth: 2)
-                                }
-                        }
-                        Button {
-                            
-                        } label: {
-                            Text("VTach")
-                                .frame(maxWidth: .infinity, minHeight: 44, maxHeight: 44)
-                                .background(.backgroundRed500)
-                                .foregroundStyle(.white)
-                                .font(.system(size: 14))
-                                .bold()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 16).stroke(Color(.borderRed600), lineWidth: 2)
-                                }
-                        }
-                    }
-                    Text("NON-SHOCKABLE RHYTHMS")
-                        .foregroundStyle(.textBlue600)
-                        .font(.system(size: 12))
-                        .bold()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    HStack {
-                        Button {
-                            
-                        } label: {
-                            Text("PEA")
-                                .frame(maxWidth: .infinity, minHeight: 44, maxHeight: 44)
-                                .background(.backgroundGray300)
-                                .foregroundStyle(.textGray900)
-                                .font(.system(size: 14))
-                                .bold()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 16).stroke(Color(.borderGray400), lineWidth: 2)
-                                }
-                        }
-                        Button {
-                            
-                        } label: {
-                            Text("Asystole")
-                                .frame(maxWidth: .infinity, minHeight: 44, maxHeight: 44)
-                                .background(.backgroundGray300)
-                                .foregroundStyle(.textGray900)
-                                .font(.system(size: 14))
-                                .bold()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 16).stroke(Color(.borderGray400), lineWidth: 2)
-                                }
-                        }
-                    }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(.horizontal, 16)
+                .onReceive(timer) { date in
+                    timeElapsed = Int(date.timeIntervalSince(currentDate))
                 }
-                .frame(maxWidth: .infinity)
-                .padding(16)
-                .background(.backgroundGray100)
-                .clipShape(RoundedRectangle(cornerRadius: 24))
-                .overlay {
-                    RoundedRectangle(cornerRadius: 24).stroke(Color(.borderGray300), lineWidth: 2)
-                }
-                Spacer()
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .padding(.horizontal, 16)
-            .onReceive(timer) { date in
-                timeElapsed = Int(date.timeIntervalSince(currentDate))
             }
         }
     }
