20c94937bd4c128e8a9d4d71c4c6ca91017116ff
Lawrence Gimenez <law@gmnz.xyz>
2026-01-14 08:07:58 +0800

    More shock popup UI improvements

--- CodeBlueHero/Views/Shock/ShockView.swift
+++ CodeBlueHero/Views/Shock/ShockView.swift
@@ -18,6 +18,35 @@ struct ShockView: View {
                 .background(Circle()
                     .fill(Color(.backgroundOrange50)))
             Text("Shock")
+                .font(.system(size: 24))
+                .foregroundStyle(.textGray900)
+                .bold()
+            HStack {
+                Button {
+                    
+                } label: {
+                    Text("Shocks Given:")
+                        .font(.system(size: 12))
+                        .foregroundStyle(.textGray600)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .background(.backgroundGray100)
+                        .clipShape(Capsule())
+                }
+                Button {
+                    
+                } label: {
+                    Text("Next: Shock:")
+                        .foregroundStyle(.white)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .background(.backgroundOrange500)
+                        .font(.system(size: 12))
+                        .clipShape(Capsule())
+                }
+            }
+            .padding(.top, 12)
             Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
