bf80e4312f6a8a2016b9829f6f5ac4663ad26138
Lawrence Gimenez <law@gmnz.xyz>
2026-01-15 08:19:11 +0800

    WIP: Clear team shock UI

--- CodeBlueHero/Views/Shock/ShockView.swift
+++ CodeBlueHero/Views/Shock/ShockView.swift
@@ -7,10 +7,18 @@
 
 import SwiftUI
 
+enum ShockSteps {
+    case select
+    case clearTeam
+    case clearPatient
+}
+
 struct ShockView: View {
     
     @Binding var isPresentShock: Bool
     @Binding var shockCount: Int
+    @State private var shockStep: ShockSteps = .clearTeam
+    @State private var shockSelected = ""
     
     var body: some View {
         VStack {
@@ -51,80 +59,109 @@ struct ShockView: View {
             }
             .padding(.top, 12)
             .padding(.bottom, 24)
-            Text("Select energy level for defibrillation")
-                .font(.system(size: 14))
-                .foregroundStyle(.textGray500)
+            if shockStep == .select {
+                Text("Select energy level for defibrillation")
+                    .font(.system(size: 14))
+                    .foregroundStyle(.textGray500)
+                    .padding(.bottom, 24)
+                Text("Biphasic (Recommended)")
+                    .foregroundStyle(.textOrange600)
+                    .font(.system(size: 12))
+                    .bold()
+                    .textCase(.uppercase)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 4)
+                Button {
+                    withAnimation {
+                        shockStep = .clearTeam
+                        shockSelected = "120J Biphasic"
+                    }
+                } label: {
+                    Text("120 J \n\(Text("Biphasic").foregroundStyle(.textGray500).font(.system(size: 12)))")
+                        .font(.system(size: 24))
+                        .bold()
+                        .foregroundStyle(.textGray700)
+                        .padding(20)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            RoundedRectangle(cornerRadius: 16).fill(.backgroundGray50)
+                                .stroke(Color(.borderGray200), lineWidth: 2)
+                        )
+                }
+                Button {
+                    withAnimation {
+                        shockStep = .clearTeam
+                        shockSelected = "150J Biphasic"
+                    }
+                } label: {
+                    Text("150 J \n\(Text("Biphasic").foregroundStyle(.textGray500).font(.system(size: 12)))")
+                        .font(.system(size: 24))
+                        .bold()
+                        .foregroundStyle(.textGray700)
+                        .padding(20)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            RoundedRectangle(cornerRadius: 16).fill(.backgroundGray50)
+                                .stroke(Color(.borderGray200), lineWidth: 2)
+                        )
+                }
+                Button {
+                    withAnimation {
+                        shockStep = .clearTeam
+                        shockSelected = "200J Biphasic"
+                    }
+                } label: {
+                    Text("200 J \n\(Text("Biphasic (Standard)").foregroundStyle(.textGray500).font(.system(size: 12)))")
+                        .font(.system(size: 24))
+                        .bold()
+                        .foregroundStyle(.textGray700)
+                        .padding(20)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            RoundedRectangle(cornerRadius: 16).fill(.backgroundGray50)
+                                .stroke(Color(.borderGray200), lineWidth: 2)
+                        )
+                }
+                Text("Monophasic")
+                    .foregroundStyle(.textGray500)
+                    .font(.system(size: 12))
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .textCase(.uppercase)
+                    .padding(.top, 8)
+                    .padding(.bottom, 4)
+                Button {
+                    withAnimation {
+                        shockStep = .clearTeam
+                        shockSelected = "360J Monophasic"
+                    }
+                } label: {
+                    Text("360 J \n\(Text("Monophasic").foregroundStyle(.textGray500).font(.system(size: 12)))")
+                        .font(.system(size: 24))
+                        .bold()
+                        .foregroundStyle(.textGray700)
+                        .padding(20)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            RoundedRectangle(cornerRadius: 16).fill(.backgroundGray50)
+                                .stroke(Color(.borderGray200), lineWidth: 2)
+                        )
+                }
+            } else if shockStep == .clearTeam {
+                // Team Clear
+                VStack {
+                    HStack {
+                        Text("\(shockSelected) Selected")
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .padding(20)
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.backgroundOrange50)
+                        .stroke(Color(.backgroundOrange500), lineWidth: 1)
+                )
                 .padding(.bottom, 24)
-            Text("Biphasic (Recommended)")
-                .foregroundStyle(.textOrange600)
-                .font(.system(size: 12))
-                .bold()
-                .textCase(.uppercase)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 4)
-            Button {
-                
-            } label: {
-                Text("120 J \n\(Text("Biphasic").foregroundStyle(.textGray500).font(.system(size: 12)))")
-                    .font(.system(size: 24))
-                    .bold()
-                    .foregroundStyle(.textGray700)
-                    .padding(20)
-                    .frame(maxWidth: .infinity)
-                    .background(
-                        RoundedRectangle(cornerRadius: 16).fill(.backgroundGray50)
-                            .stroke(Color(.borderGray200), lineWidth: 2)
-                    )
-            }
-            Button {
-                
-            } label: {
-                Text("150 J \n\(Text("Biphasic").foregroundStyle(.textGray500).font(.system(size: 12)))")
-                    .font(.system(size: 24))
-                    .bold()
-                    .foregroundStyle(.textGray700)
-                    .padding(20)
-                    .frame(maxWidth: .infinity)
-                    .background(
-                        RoundedRectangle(cornerRadius: 16).fill(.backgroundGray50)
-                            .stroke(Color(.borderGray200), lineWidth: 2)
-                    )
-            }
-            Button {
-                
-            } label: {
-                Text("200 J \n\(Text("Biphasic (Standard)").foregroundStyle(.textGray500).font(.system(size: 12)))")
-                    .font(.system(size: 24))
-                    .bold()
-                    .foregroundStyle(.textGray700)
-                    .padding(20)
-                    .frame(maxWidth: .infinity)
-                    .background(
-                        RoundedRectangle(cornerRadius: 16).fill(.backgroundGray50)
-                            .stroke(Color(.borderGray200), lineWidth: 2)
-                    )
-            }
-            Text("Monophasic")
-                .foregroundStyle(.textGray500)
-                .font(.system(size: 12))
-                .bold()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .textCase(.uppercase)
-                .padding(.top, 8)
-                .padding(.bottom, 4)
-            Button {
-                
-            } label: {
-                Text("360 J \n\(Text("Monophasic").foregroundStyle(.textGray500).font(.system(size: 12)))")
-                    .font(.system(size: 24))
-                    .bold()
-                    .foregroundStyle(.textGray700)
-                    .padding(20)
-                    .frame(maxWidth: .infinity)
-                    .background(
-                        RoundedRectangle(cornerRadius: 16).fill(.backgroundGray50)
-                            .stroke(Color(.borderGray200), lineWidth: 2)
-                    )
             }
             Spacer()
             Button {
